06398caee22c0d54a949d83f28e0c7574885bd1a
eunsu14 <7266706@naver.com>
2023-11-03 18:39:40 +0900

    Update student20210769.py

--- HW1/student20210769.py
+++ HW1/student20210769.py
@@ -6,7 +6,7 @@ import math
 wb = load_workbook(filename = 'student.xlsx')
 ws = wb['Sheet1']
 
-list = []
+total_list = []
 for row in range(2, ws.max_row + 1):
     mid = ws.cell(row = row, column = 3).value
     final = ws.cell(row = row, column = 4).value
@@ -15,47 +15,90 @@ for row in range(2, ws.max_row + 1):
 
     # total
     total  = mid * 0.3 + final * 0.35 + hw * 0.34 + attendance
-    list.append(total) # list에 total 값 넣음
+    total_list.append(total) # list에 total 값 넣음
     ws.cell(row = row, column = 7, value = total)
 
 # 순위
-# sorted_score = sorted(score, reverse = True) 
-# ranks = [sorted_score.index(s) + 1 for s in score]
+rank = [sorted(total_list, reverse=True).index(i) + 1 for i in total_list]
 
-rank = [sorted(list, reverse=True).index(i) + 1 for i in list]
+stuNum = ws.max_row - 1
 
-#A, B, C 최대 몇 명 받을 수 있는지 계산
-maxA = math.trunc((ws.max_row - 1) * 0.3)
-maxB = math.trunc((ws.max_row - 1) * 0.7)
+a = math.trunc(stuNum * 0.15)
+n = 0
+i = 1
+while a > 0:        
+    if (rank.count(i) <= a):
+        num = list(filter(lambda x : rank[x] == i, range(len(rank))))
+        a -= rank.count(i)
+        n += rank.count(i)
+        for j in num:
+            rank[j] = 'A+'
+    else:
+        break
+    i += 1
 
-# 동점자 계산
-stuNum = ws.max_row - 1 
-maxNum_A = math.trunc(stuNum * 0.15) # 11등
-maxNum_A0 = math.trunc(stuNum * 0.3) # 22등
-maxNum_B = math.trunc(stuNum * 0.5) # 37등
-maxNum_B0 = math.trunc(stuNum * 0.7) # 51등
-maxNum_C = math.trunc(stuNum * 0.85) # 62등
+a0 = math.trunc(stuNum * 0.3) - n
+while a0 > 0:
+    if (rank.count(i) <= a0):
+        num = list(filter(lambda x : rank[x] == i, range(len(rank))))
+        a0 -= rank.count(i)
+        for j in num:
+            rank[j] = 'A0'
+    else:
+        break
+    i += 1
+
+n = 0
+b = math.trunc(stuNum * 0.2)
+while b > 0:
+    if (rank.count(i) <= b):
+        num = list(filter(lambda x : rank[x] == i, range(len(rank))))
+        b -= rank.count(i)
+        n += rank.count(i)
+        for j in num:
+            rank[j] = 'B+'
+    else:
+        break
+    i += 1
+
+b0 = math.trunc(stuNum * 0.4) - n
+while b0 > 0:
+    if (rank.count(i) <= b0):
+        num = list(filter(lambda x : rank[x] == i, range(len(rank))))
+        b0 -= rank.count(i)
+        for j in num:
+            rank[j] = 'B0'
+    else:
+        break
+    i += 1
+
+n = 0
+c = math.trunc(stuNum * 0.15)
+while c > 0:
+    if (rank.count(i) <= c):
+        num = list(filter(lambda x : rank[x] == i, range(len(rank))))
+        c -= rank.count(i)
+        n += rank.count(i)
+        for j in num:
+            rank[j] = 'C+'
+    else:
+        break
+    i += 1
+
+for i in range(len(rank)):
+    num = list(filter(lambda x : rank[x] == i, range(len(rank))))
+    for j in num:
+        rank[j] = 'C0'
+    i += 1
+
+k = 0
+for k in range(len(rank)):
+    if ((ws.cell(row = k + 2, column = 6).value) == 0 or (ws.cell(row = k + 2, column = 7).value) < 40):
+        rank[k] = 'F'
 
 i = 0
-garde = ''
-gradeList = []
 for row in range(2, ws.max_row + 1):
-    if ((rank[i] <= maxA / 2) and (rank.count(rank[i]) <= maxNum_A)):
-        grade = 'A+'
-    elif ((rank[i] <= maxA) and (rank.count(rank[i]) <= maxA)):
-        grade = 'A0'
-    elif ((rank[i] <= (ws.max_row - 1) * 0.5) and ((rank.count(rank[i]) <= maxNum_B - maxNum_A0))):
-        grade = 'B+'
-    elif (rank[i] <= maxB and (rank.count(rank[i]) <= maxB)):
-        grade = 'B0'
-    elif ((rank[i] < (ws.max_row - 1) * 0.85) and ((rank.count(rank[i]) <= maxNum_C - maxNum_B0))):
-        grade = 'C+'
-    else:
-        grade = 'C0'
-    if ((ws.cell(row = row, column = 6).value)== 0 or (ws.cell(row = row, column = 7).value) < 40):
-        grade = 'F'
-    ws.cell(row = row, column = 8, value = grade)
-    gradeList.append(grade)
+    ws.cell(row = row, column = 8, value = rank[i])
     i += 1
 
 wb.save(filename = 'student.xlsx')
